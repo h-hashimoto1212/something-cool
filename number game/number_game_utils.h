2a8156e16c07af3b5415fbf32e0619e7b36d846d
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2020-06-18 18:26:24 +0900

    clear screen & invalid value

--- number game/number_game_utils.h
+++ number game/number_game_utils.h
@@ -1,7 +1,13 @@
 #include <vector>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <ctime>
 
 #ifndef NUMBER_GAME_UTILS
 #define NUMBER_GAME_UTILS
+
+void clear_screen();
 
 void print_vector(std::vector<int> vector, int score);
 
